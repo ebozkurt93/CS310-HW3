e317de0cd9d7b46a4a887880e561826d48c0693a
Erdem Bozkurt <ebozkurt@sabanciuniv.edu>
2017-05-02 12:03:51 +0300

    Initial Commit

---
+++ app/src/main/java/edu/sabanciuniv/newsstarterapp/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.sabanciuniv.newsstarterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher);
+    }
+}
